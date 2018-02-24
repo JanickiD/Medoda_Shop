8e460f9231183b0a12f76b9fdd7e475270920b30
Norbert <norbert.suchojad@gmail.com>
2018-02-24 15:39:18 +0100

    ns

--- src/main/java/pl/pwn/reaktor/medoda/service/UserService.java
+++ src/main/java/pl/pwn/reaktor/medoda/service/UserService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package pl.pwn.reaktor.medoda.service;
 
 import java.util.ArrayList;
@@ -136,58 +135,3 @@ public class UserService {
 	
 
 }
-=======
-//package pl.pwn.reaktor.medoda.service;
-//
-//import java.util.Objects;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//import pl.pwn.reaktor.medoda.model.Role;
-//import pl.pwn.reaktor.medoda.model.User;
-//import pl.pwn.reaktor.medoda.repository.RoleRepository;
-//import pl.pwn.reaktor.medoda.repository.UserRepository;
-//
-//
-//@Service
-//public class UserService {
-//	
-//	private UserRepository userRepository;
-//	private BCryptPasswordEncoder bCryptPasswordEncoder;
-//	private RoleRepository roleRepository;
-//	
-//	@Autowired
-//	public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder,
-//			RoleRepository roleRepository) {
-//		super();
-//		this.userRepository = userRepository;
-//		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-//		this.roleRepository = roleRepository;
-//	}
-//	
-//	
-//	public User addUserWithRoleUser(User user) {
-//
-//		user.setActive(true);
-//
-//		Role role = getUserRole();
-//
-//		user.setRole(role);
-//		user.setPass(bCryptPasswordEncoder.encode(user.getPass()));
-//
-//		return userRepository.save(user);
-//	}
-//	
-//	private Role getUserRole() {
-//		Role role = roleRepository.findByRole("User");
-//		if (Objects.isNull(role)) {
-//			role = roleRepository.save(new Role("User"));
-//		}
-//		return role;
-//	}
-//	
-//
-//}
->>>>>>> 9f4fa9eeb3b79b7035b69f16926919e5e5bd6fec
