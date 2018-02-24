ebcfb28953cfaed96c349ee21693e0281e4d661f
JanickiD <janicki.damian@wp.pl>
2018-02-24 10:16:58 +0100

    initialize

---
+++ src/main/java/pl/pwn/reaktor/medoda/MedodaShopApplication.java
@@ -0,0 +1,12 @@
+package pl.pwn.reaktor.medoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedodaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedodaShopApplication.class, args);
+	}
+}
