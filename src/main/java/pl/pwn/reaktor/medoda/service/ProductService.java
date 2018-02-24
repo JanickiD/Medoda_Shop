1b0153fbd26a8a9c5981a8530390e9a76f3777d3
MSamiczak <maciek.samiczak@gmail.com>
2018-02-24 15:16:27 +0100

    # Maciek produkty

---
+++ src/main/java/pl/pwn/reaktor/medoda/service/ProductService.java
@@ -0,0 +1,34 @@
+package pl.pwn.reaktor.medoda.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pl.pwn.reaktor.medoda.model.Product;
+import pl.pwn.reaktor.medoda.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	public ProductService(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+
+	public List<Product> listAllProducts() {
+
+		return productRepository.findAll();
+	}
+
+	public Product findById(long id) {
+
+		return productRepository.findOne(id);
+	}
+	
+	
+	
+
+}
