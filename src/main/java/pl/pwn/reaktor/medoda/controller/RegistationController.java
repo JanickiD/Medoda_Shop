a9f7e988f27e4f183c1b9026be75827ec8968918
JanickiD <janicki.damian@wp.pl>
2018-02-24 13:59:23 +0100

    rejestracja -> początek prac

---
+++ src/main/java/pl/pwn/reaktor/medoda/controller/RegistationController.java
@@ -0,0 +1,42 @@
+package pl.pwn.reaktor.medoda.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import pl.pwn.reaktor.medoda.model.User;
+
+
+@Controller
+public class RegistationController {
+
+	@GetMapping("/registration")
+	private String registation() {
+		return("/registration");
+	}
+	
+	@PostMapping("/register")
+	public String save(@Valid @ModelAttribute User user, BindingResult bindingResult, Model model) {
+
+		if (bindingResult.hasErrors()) {
+
+		}
+
+		User savedUser = userService.addUserWithRole(user);
+		System.out.println("dodany: " + savedUser);
+
+		model.addAttribute("status", "Rejestracja pomyślna!");
+		model.addAttribute("user", new User());
+
+		return "registration";
+
+	}
+	
+	
+	
+}
